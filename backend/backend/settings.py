8bc34d24e3935fa3c494253f02b4db732d82ab19
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-07-05 11:19:23 +0100

    staticfiles added for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+# Ruta donde se recogen los archivos estáticos para despliegues (ej. 'python manage.py collectstatic')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Carpetas adicionales donde Django buscará archivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
