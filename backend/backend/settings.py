465109f7dbdc19c5ac7e6cbbef1ba1c21594ef74
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2024-07-05 11:30:41 +0100

    aver si ahora si

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,9 +126,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Carpetas adicionales donde Django buscará archivos estáticos
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
